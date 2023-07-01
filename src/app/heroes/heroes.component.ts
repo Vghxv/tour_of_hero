25f1c37837f115f7b63eb87db223a55e5841b330
vincent <hankggg2003xd@gmail.com>
2023-07-01 18:56:42 +0800

    finish message service and hero service

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,26 +1,39 @@
 import { Component } from '@angular/core';
+
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
+
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent {
-  constructor(private heroService: HeroService) {}
+  selectedHero?: Hero;
   
   heroes: Hero[] = [];
   
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
+  // getHeroes(): void {
+  //   this.heroes = this.heroService.getHeroes();
+  // }
+  //   Observable.subscribe() is the critical difference.
+  // The previous version assigns an array of heroes to the component's heroes property. The assignment occurs synchronously, as if the server could return heroes instantly or the browser could freeze the UI while it waited for the server's response.
+  // That won't work when the HeroService is actually making requests of a remote server.
+  
   getHeroes(): void {
-    this.heroes = this.heroService.getHeroes();
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
   }
 
   ngOnInit(): void {
     this.getHeroes();
   }
-  selectedHero?: Hero;
-
+  
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);  }
   }
-}
+  
